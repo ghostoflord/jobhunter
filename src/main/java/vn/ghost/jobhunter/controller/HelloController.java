9c1c0ebac976ba1606bf9ccc2536897bd688b983
ghostoflord <ghost2892003zzz@gmail.com>
2024-08-30 16:27:21 +0700

    41 create exception

--- src/main/java/vn/ghost/jobhunter/controller/HelloController.java
+++ src/main/java/vn/ghost/jobhunter/controller/HelloController.java
@@ -3,11 +3,15 @@ package vn.ghost.jobhunter.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.ghost.jobhunter.service.error.IdInvalidException;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String getHelloWorld() {
+    public String getHelloWorld() throws IdInvalidException {
+        if (true)
+            throw new IdInvalidException("check exception homepage");
         return "Hello World ";
     }
 }
--- src/main/java/vn/ghost/jobhunter/controller/UserController.java
+++ src/main/java/vn/ghost/jobhunter/controller/UserController.java
@@ -1,14 +1,11 @@
 package vn.ghost.jobhunter.controller;
 
 import java.util.List;
-// import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
