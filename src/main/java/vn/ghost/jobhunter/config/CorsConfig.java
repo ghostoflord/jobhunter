df1e61816f5c26749b09e36aa905edb72fd378f9
ghostoflord <ghost2892003zzz@gmail.com>
2024-09-17 15:46:14 +0700

    90 config backend connect frontend

--- src/main/java/vn/ghost/jobhunter/config/CorsConfig.java
+++ src/main/java/vn/ghost/jobhunter/config/CorsConfig.java
@@ -14,13 +14,19 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:3000", "http://localhost:4173", "http://localhost:5173"));
+        // các methob nào được kết nối
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
+        // các phần header được phép gửi lên
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        // có gửi kèm cookies hay không
         configuration.setAllowCredentials(true);
+        // thời gian pe-fight request có thể cache
         configuration.setMaxAge(3600L);
         // How long the response from a pre-flight request can be cached by clients
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // cấu hình cors cho tất cả các api
         source.registerCorsConfiguration("/**", configuration);
         // Apply this configuration to all paths
         return source;
--- src/main/java/vn/ghost/jobhunter/controller/AuthController.java
+++ src/main/java/vn/ghost/jobhunter/controller/AuthController.java
@@ -92,20 +92,23 @@ public class AuthController {
 
         @GetMapping("/auth/account")
         @ApiMessage("fetch account")
-        public ResponseEntity<ResLoginDTO.UserLogin> getAccount() {
+        public ResponseEntity<ResLoginDTO.UserGetAccount> getAccount() {
                 String email = SecurityUtil.getCurrentUserLogin().isPresent()
                                 ? SecurityUtil.getCurrentUserLogin().get()
                                 : "";
 
                 User currentUserDB = this.userService.handleGetUserByUsername(email);
                 ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin();
+                ResLoginDTO.UserGetAccount userGetAccount = new ResLoginDTO.UserGetAccount();
+
                 if (currentUserDB != null) {
                         userLogin.setId(currentUserDB.getId());
                         userLogin.setEmail(currentUserDB.getEmail());
                         userLogin.setName(currentUserDB.getName());
+                        userGetAccount.setUser(userLogin);
                 }
 
-                return ResponseEntity.ok().body(userLogin);
+                return ResponseEntity.ok().body(userGetAccount);
         }
 
         @GetMapping("/auth/refresh")
@@ -162,7 +165,7 @@ public class AuthController {
         }
 
         @PostMapping("/auth/logout")
-        @ApiMessage("Logout User Success")
+        @ApiMessage("Logout User")
         public ResponseEntity<Void> logout() throws IdInvalidException {
                 String email = SecurityUtil.getCurrentUserLogin().isPresent() ? SecurityUtil.getCurrentUserLogin().get()
                                 : "";
@@ -187,5 +190,4 @@ public class AuthController {
                                 .header(HttpHeaders.SET_COOKIE, deleteSpringCookie.toString())
                                 .body(null);
         }
-
 }
