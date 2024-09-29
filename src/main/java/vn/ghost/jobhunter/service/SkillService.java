c5130ae4cc7556f2caa7aed7db1b4671ccc260d0
ghostoflord <ghost2892003zzz@gmail.com>
2024-09-29 22:09:00 +0700

    122 CRUD Subscribers

--- src/main/java/vn/ghost/jobhunter/service/SkillService.java
+++ src/main/java/vn/ghost/jobhunter/service/SkillService.java
@@ -35,7 +35,7 @@ public class SkillService {
     }
 
     public Skill updateSkill(Skill s) {
-        
+
         return this.skillRepository.save(s);
     }
 
@@ -44,6 +44,9 @@ public class SkillService {
         Optional<Skill> skillOptional = this.skillRepository.findById(id);
         Skill currentSkill = skillOptional.get();
         currentSkill.getJobs().forEach(job -> job.getSkills().remove(currentSkill));
+
+        // delete subscriber (inside subscriber_skill table)
+        currentSkill.getSubscribers().forEach(subs -> subs.getSkills().remove(currentSkill));
 
         // delete skill
         this.skillRepository.delete(currentSkill);
