e358d69ddfd5863efc7c803a9b8462272b6e3f23
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-19 17:10:56 +0700

    corn job

--- src/main/java/vn/ghost/jobhunter/controller/EmailController.java
+++ src/main/java/vn/ghost/jobhunter/controller/EmailController.java
@@ -1,9 +1,11 @@
 package vn.ghost.jobhunter.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import vn.ghost.jobhunter.service.EmailService;
 import vn.ghost.jobhunter.service.SubscriberService;
 import vn.ghost.jobhunter.util.annotation.ApiMessage;
@@ -13,15 +15,17 @@ import vn.ghost.jobhunter.util.annotation.ApiMessage;
 public class EmailController {
 
     private final EmailService emailService;
-      private final SubscriberService subscriberService;
+    private final SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService,SubscriberService subscriberService) {
+    public EmailController(EmailService emailService, SubscriberService subscriberService) {
         this.emailService = emailService;
-        this.subscriberService=subscriberService;
+        this.subscriberService = subscriberService;
     }
 
     @GetMapping("/email")
     @ApiMessage("Send simple email")
+    // @Scheduled(cron = "*/30 * * * * *")
+    // @Transactional
     public String sendSimpleEmail() {
         // this.emailService.sendSimpleEmail();
         this.subscriberService.sendSubscribersEmailJobs();
