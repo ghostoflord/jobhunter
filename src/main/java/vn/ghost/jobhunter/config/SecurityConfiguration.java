eb3b6f22166dc62cd1a46b36c92ae3f094d2500b
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-19 21:27:13 +0700

    132. Swagger basic

--- src/main/java/vn/ghost/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/ghost/jobhunter/config/SecurityConfiguration.java
@@ -47,6 +47,10 @@ public class SecurityConfiguration {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**", "/api/v1/auth/register",
                 "/api/v1/email/**",
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"
+
         };
 
         http
