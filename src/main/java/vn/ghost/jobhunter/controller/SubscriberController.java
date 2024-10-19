a52266e515b648eef709f0cd8b62e294f0f87394
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-19 20:54:56 +0700

    130 update

--- src/main/java/vn/ghost/jobhunter/controller/SubscriberController.java
+++ src/main/java/vn/ghost/jobhunter/controller/SubscriberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import vn.ghost.jobhunter.domain.Subscriber;
 import vn.ghost.jobhunter.service.SubscriberService;
+import vn.ghost.jobhunter.util.SecurityUtil;
 import vn.ghost.jobhunter.util.annotation.ApiMessage;
 import vn.ghost.jobhunter.util.error.IdInvalidException;
 
@@ -44,6 +45,16 @@ public class SubscriberController {
             throw new IdInvalidException("Id " + subsRequest.getId() + " không tồn tại");
         }
         return ResponseEntity.ok().body(this.subscriberService.update(subsDB, subsRequest));
+    }
+
+    @PostMapping("/subscribers/skills")
+    @ApiMessage("Get subscriber's skill")
+    public ResponseEntity<Subscriber> getSubscribersSkill() throws IdInvalidException {
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        return ResponseEntity.ok().body(this.subscriberService.findByEmail(email));
     }
 
 }
--- src/main/java/vn/ghost/jobhunter/repository/SubscriberRepository.java
+++ src/main/java/vn/ghost/jobhunter/repository/SubscriberRepository.java
@@ -9,6 +9,6 @@ import vn.ghost.jobhunter.domain.Subscriber;
 @Repository
 public interface SubscriberRepository extends JpaRepository<Subscriber, Long>,
                 JpaSpecificationExecutor<Subscriber> {
-
         boolean existsByEmail(String email);
+        Subscriber findByEmail(String email);
 }
